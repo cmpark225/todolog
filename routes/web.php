f860ee7a8e5de25384c898054fce9e168434fa89
cmpark <cmpark@workmob.co.kr>
2016-11-22 09:52:37 +0900

    메인 tasks 뷰 생성

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
+use Illuminate\Http\Request;
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+/**
+ * Add New Task
+ */
+
+Route::post('/task', function (Request $request){
+
+});
+
+/**
+ * Delete Task
+ */
+
+Route::delete('/task/{task}', function (Task $task){
+
+
 });
